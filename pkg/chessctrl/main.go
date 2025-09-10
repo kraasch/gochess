01308e337f871e71bbfaf26f80439419e862e7fe
Alex Kraasch <alex@kraasch.eu>
2025-09-10 22:23:09 +0200

    Implement move pieces.

--- pkg/chessctrl/main.go
+++ pkg/chessctrl/main.go
@@ -1,9 +1,93 @@
 package chessctrl
 
+import (
+	"strconv"
+	"strings"
+)
+
 const (
 	NL2 = "\n" // TODO: generalize end-of-line sequence.
 )
 
-func Toast() string {
-	return "Toast!"
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func flipGrid(input string) string {
+	// Split the input into lines.
+	lines := strings.Split(input, "\n")
+	// Reverse the order of lines.
+	for i, j := 0, len(lines)-1; i < j; i, j = i+1, j-1 {
+		lines[i], lines[j] = lines[j], lines[i]
+	}
+	// Reverse each line.
+	for i := range lines {
+		lines[i] = reverseString(lines[i])
+	}
+	// Join the lines back into a string.
+	return strings.Join(lines, "\n")
+}
+
+func Flip(board string) string {
+	return flipGrid(board)
+}
+
+func readCharAt(s *string, x, y int) string {
+	// Assumes x and y are within bounds.
+	ss := *s
+	lines := strings.Split(ss, "\n")
+	line := lines[y]
+	runes := []rune(line)
+	return string(runes[x])
+}
+
+func replaceCharAt(s *string, x, y int, insert string) {
+	// Assumes x and y are within bounds.
+	// Assumes insert is exactly of length 1.
+	bytes := []byte(*s)
+	bytes[x+(y*8)+y] = insert[0]
+	*s = string(bytes)
+}
+
+// TODO: make this return an error, if move is invalid.
+// TODO: 1) not a valid move string (invalid length, ie not equal).
+// TODO: 2) not a valid move string (origin or destination invalid).
+// TODO: 3) no piece at origin.
+func validate(board *string, move string) bool {
+	hasCorrectLength := true
+	followsRegex := true
+	hasPiece := true
+	return hasCorrectLength && followsRegex && hasPiece
+}
+
+func apply(board *string, move string) {
+	// origin = o, destination = d, alphabetic = a; numeric = n.
+	// a simple move instruction in chess follows the form:
+	// oa + on + da + dn, ie A1B2 moves a piece from A1 to B2.
+	upper := strings.ToUpper(move) // use upper case in order to subtract ASCII A = 65 from alphabetic coordinates.
+	oa := int(upper[0]) - 65
+	on, _ := strconv.Atoi(upper[1:2])
+	on = 7 - (on - 1)
+	da := int(upper[2]) - 65
+	dn, _ := strconv.Atoi(upper[3:4])
+	dn = 7 - (dn - 1)
+	origin := readCharAt(board, oa, on)
+	replaceCharAt(board, oa, on, " ")
+	replaceCharAt(board, da, dn, origin)
+}
+
+func Move(board *string, moves string) {
+	// TODO: Assumes moves are split by commas. (make tests).
+	movesArr := strings.Split(moves, ",")
+	for _, move := range movesArr {
+		if validate(board, move) {
+			// fmt.Printf("Run %d, move '%s'\n", i, move)
+			// fmt.Printf("-----------\n%s\n-----------\n", board)
+			apply(board, move)
+		}
+	}
 }
--- pkg/chessctrl/main_test.go
+++ pkg/chessctrl/main_test.go
@@ -28,19 +28,20 @@ type TestSuite struct {
 
 var suites = []TestSuite{
 	/*
-	 * Test for Toast()
+	 * Test errors of Move()
 	 */
-	{
+	{ // TODO: implement.
 		testingFunction: func(in TestList) (out string) {
-			// board := in.inputArr[0]
-			out = Toast()
+			board := in.inputArr[0]
+			moves := in.inputArr[1]
+			Move(&board, moves)
+			out = board
 			return out
 		},
 		tests: []TestList{
 			{
-				// turn in to these: ♔♕♖♗♘♙ ♚♛♜♝♞♟
-				testName: "controller_flip_00",
-				isMulti:  false,
+				testName: "controller_move_one-move_00",
+				isMulti:  true,
 				inputArr: []string{
 					"rnbqkbnr" + NL +
 						"pppppppp" + NL +
@@ -50,18 +51,164 @@ var suites = []TestSuite{
 						"        " + NL +
 						"PPPPPPPP" + NL +
 						"RNBQKBNR",
-					"pieces",
+					"a7a6",
 				},
-				expectedValue:// this comment prevents start of string literal here.
-				"Toast!",
-				// "♜♞♝♛♚♝♞♜" + NL +
-				// 	"♟♟♟♟♟♟♟♟" + NL +
-				// 	"        " + NL +
-				// 	"        " + NL +
-				// 	"        " + NL +
-				// 	"        " + NL +
-				// 	"♙♙♙♙♙♙♙♙" + NL +
-				// 	"♖♘♗♕♔♗♘♖",
+				expectedValue: // this comment prevents start of string literal here.
+				"rnbqkbnr" + NL +
+					" ppppppp" + NL +
+					"p       " + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"PPPPPPPP" + NL +
+					"RNBQKBNR",
+			},
+		},
+	},
+
+	/*
+	 * Test for Move()
+	 */
+	{
+		testingFunction: func(in TestList) (out string) {
+			board := in.inputArr[0]
+			moves := in.inputArr[1]
+			Move(&board, moves)
+			out = board
+			return out
+		},
+		tests: []TestList{
+			{
+				testName: "controller_move_one-move_00",
+				isMulti:  true,
+				inputArr: []string{
+					"rnbqkbnr" + NL +
+						"pppppppp" + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"PPPPPPPP" + NL +
+						"RNBQKBNR",
+					"a7a6",
+				},
+				expectedValue: // this comment prevents start of string literal here.
+				"rnbqkbnr" + NL +
+					" ppppppp" + NL +
+					"p       " + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"PPPPPPPP" + NL +
+					"RNBQKBNR",
+			},
+			{
+				testName: "controller_move_multiple-moves_00",
+				isMulti:  true,
+				inputArr: []string{
+					"rnbqkbnr" + NL +
+						"pppppppp" + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"PPPPPPPP" + NL +
+						"RNBQKBNR",
+					"a7a6,b7b6",
+				},
+				expectedValue: // this comment prevents start of string literal here.
+				"rnbqkbnr" + NL +
+					"  pppppp" + NL +
+					"pp      " + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"PPPPPPPP" + NL +
+					"RNBQKBNR",
+			},
+			{
+				testName: "controller_move_multiple-moves_01",
+				isMulti:  true,
+				inputArr: []string{
+					"rnbqkbnr" + NL +
+						"pppppppp" + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"PPPPPPPP" + NL +
+						"RNBQKBNR",
+					"a7a6,a6a5",
+				},
+				expectedValue: // this comment prevents start of string literal here.
+				"rnbqkbnr" + NL +
+					" ppppppp" + NL +
+					"        " + NL +
+					"p       " + NL +
+					"        " + NL +
+					"        " + NL +
+					"PPPPPPPP" + NL +
+					"RNBQKBNR",
+			},
+		},
+	},
+
+	/*
+	 * Test for Flip()
+	 */
+	{
+		testingFunction: func(in TestList) (out string) {
+			board := in.inputArr[0]
+			out = Flip(board)
+			return out
+		},
+		tests: []TestList{
+			{
+				testName: "controller_flip_00",
+				isMulti:  true,
+				inputArr: []string{
+					"rnbqkbnr" + NL +
+						"pppppppp" + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"PPPPPPPP" + NL +
+						"RNBQKBNR",
+				},
+				expectedValue: // this comment prevents start of string literal here.
+				"RNBKQBNR" + NL +
+					"PPPPPPPP" + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"pppppppp" + NL +
+					"rnbkqbnr",
+			},
+			{
+				// turn in to these: ♔♕♖♗♘♙ ♚♛♜♝♞♟
+				testName: "controller_flip_00",
+				isMulti:  true,
+				inputArr: []string{
+					"rnb     " + NL +
+						"ppp     " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        " + NL +
+						"        ",
+				},
+				expectedValue: // this comment prevents start of string literal here.
+				"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"        " + NL +
+					"     ppp" + NL +
+					"     bnr",
 			},
 		},
 	},
